da46d13a0dfd2079740c7a386eb29e5f10849439
novedice <natalia.kitesurf@gmail.com>
2025-11-20 12:38:51 +0000

    feat: implement depth validation rule

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,7 +1,8 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql } from 'graphql';
+import { graphql, parse, specifiedRules, validate } from 'graphql';
 import { Schema } from './graphQLSchema.js';
+import depthLimit from 'graphql-depth-limit';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   const { prisma } = fastify;
@@ -16,13 +17,18 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       },
     },
     async handler(req) {
-     
+      const documentAst = parse(req.body.query);
+      const validation = validate(Schema, documentAst, [...specifiedRules, depthLimit(5)])
+      if (validation.length) {
+        return {errors: validation};
+      } else {
       return graphql({
         schema: Schema,
         source: req.body.query,
         variableValues: req.body.variables,
-        contextValue: {prisma, req}
+        contextValue: {prisma, req},
       });
+    }
     },
   });
 };
