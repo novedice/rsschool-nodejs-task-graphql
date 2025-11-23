24d6a080798e72503558aaeeaf031c7c3a63bdca
novedice <natalia.kitesurf@gmail.com>
2025-11-23 22:01:37 +0000

    refactor: delete unused commented code

--- src/routes/graphql/types/userType.ts
+++ src/routes/graphql/types/userType.ts
@@ -44,34 +44,3 @@ export const UserType: GraphQLObjectType = new GraphQLObjectType({
     }
   })
 })
-
-// export const UserType: GraphQLObjectType = new GraphQLObjectType({
-//   name: "User",
-//   fields: () => ({
-//     id: { type: new GraphQLNonNull(UUIDType)},
-//     name: {type: new GraphQLNonNull(GraphQLString)},
-//     balance: {type: new GraphQLNonNull(GraphQLFloat)},
-//     profile: {type: ProfileType},
-//     posts: {type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(PostType)))},
-//     userSubscribedTo: {
-//       type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(UserType))),
-//       resolve: async (parent: parentInterface, _args, contextValue: contextType) => {
-//         const results = await contextValue.prisma.subscribersOnAuthors.findMany({
-//           where: { subscriberId: parent.id },
-//           include: { author: true }
-//         });
-//         return results.map(res => res.author);
-//       }
-//     },
-//     subscribedToUser: {
-//       type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(UserType))),
-//       resolve: async (parent: parentInterface, _args, context: contextType) => {
-//         const results = await context.prisma.subscribersOnAuthors.findMany({
-//           where: { authorId: parent.id },
-//           include: { subscriber: true }
-//         });
-//         return results.map(res => res.subscriber);
-//       }
-//     }
-//   })
-// })
